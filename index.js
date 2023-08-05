0b11010be65c9a8f8c1497f5379ba94fbd2670b8
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-05 22:53:24 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName='bob';
+function upperCaseCustomerName(){
+        return customerName=customerName.toUpperCase();
+         ;
+}
+
+function setBestCustomer(){
+    window.bestCustomer= 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+
+    window.bestCustomer ="maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer="dd";
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer ="cc";
+    
+}
